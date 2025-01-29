0bebe44a7562b88d09548f47395c1a57ce10d883
IfyQ <ifyisife@yahoo.com>
2025-01-29 11:06:31 +0000

    first commit

---
+++ src/main/java/com/IfyQ/strokeprediction/StrokepredictionApplication.java
@@ -0,0 +1,13 @@
+package com.IfyQ.strokeprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrokepredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrokepredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IfyQ/strokeprediction/StrokepredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IfyQ.strokeprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrokepredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
